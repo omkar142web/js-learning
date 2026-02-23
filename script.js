1e296dc5109cad0a208f8204a33d70ed811a4421
Omkar <omkarpc142@gmail.com>
2026-02-24 02:40:30 +0530

    hehe

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-console.log('first clg');
+// console.log('first clg');
 
 
 
-[1, 2].forEach(alert);
+// [1, 2].forEach(alert);
+
+
+// let c ,b=2,a;
+// b =a = c;
+
+// console.log(c,a,b)
+
+
+// console.log((a = b + 1))
+
+// let counter = 2;
+// let a = ++counter; 
+
+// console.log(a)
+// console.log(counter)
+
+// console.log( Number(" "))
 
 
 
 
+// let a = Number(prompt("First number?", 1));
+// let b = Number(prompt("Second number?", 2));
+
+// alert(a + b); // 12
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+let age = prompt('age?', 18);
+let message = (age < 3) ? 'Hi, baby!' :
+  (age < 18) ? 'Hello!' :
+  (age < 100) ? 'Greetings!' :
+  'What an unusual age!';
+alert( message );
 
 
 
