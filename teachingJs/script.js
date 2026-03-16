ca3b1f2ce4ad59379661d01dc642d376bec0f884
Omkar <omkarpc142@gmail.com>
2026-03-16 22:56:54 +0530

    hm

--- teachingJs/script.js
+++ teachingJs/script.js
@@ -291,19 +291,16 @@ const javascriptCourse = {
 // let i = javascriptCourse.sections[4].lessons[1].title  ;
 //  console.log(i);
 
-
 let sections_dataTypes = document.querySelector(".sections_dataTypes");
 let main = document.querySelector(".main");
 
 for (const element of javascriptCourse.sections[4].lessons) {
   console.log(element.title);
+  
   let section = document.createElement("div");
   section.classList.add("sections_dataTypes");
 
   section.innerText = element.title;
 
   main.append(section);
-
-
 }
-
