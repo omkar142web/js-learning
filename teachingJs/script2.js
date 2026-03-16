b9d3da16467752b0abe24f909b42a2362051f8ef
Omkar <omkarpc142@gmail.com>
2026-03-17 01:37:54 +0530

    hm

--- teachingJs/script2.js
+++ teachingJs/script2.js
@@ -4,7 +4,9 @@ let sections_dataTypes = document.querySelector(".sections_dataTypes");
 let main = document.querySelector(".main");
 let main_aside = document.querySelector(".main_aside");
 
-for (const element of javascriptCourse.sections[4].lessons) {
+let datatypess = javascriptCourse.sections[4].lessons;
+
+for (const element of datatypess) {
   console.log(element.title);
 
   let section = document.createElement("div");
