6a2ad64d8c365171ea09d0c8d0465d2a1d750313
Omkar <omkarpc142@gmail.com>
2026-03-16 23:47:29 +0530

    hmm

--- teachingJs/info.js
+++ teachingJs/info.js
@@ -1,56 +1,18 @@
-let para = document.querySelector('p');
+let para = document.querySelector("p");
 
-para.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae fugit
-            fugiat voluptate quis eveniet molestias illum doloribus esse! Illum
-            nobis consequatur ab recusandae a rerum veniam quibusdam hic quidem
-            accusamus! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Cum eius voluptates harum at numquam est doloremque aperiam
-            repellendus expedita sit ea sunt laborum accusantium consectetur id,
-            ipsa labore soluta aliquid debitis dignissimos doloribus in.
-            Laudantium ea ullam nisi eaque non qui, odio debitis pariatur
-            impedit maiores odit sunt temporibus minima ipsam explicabo
-            praesentium eligendi dolore dicta laborum corporis nesciunt rem vel,
-            eos tempora? Deserunt voluptatum nihil, explicabo consequatur
-            asperiores, ipsa earum unde numquam odio harum magnam in doloribus
-            sunt accusamus distinctio quidem soluta repellat illo autem. Dolores
-            dolor neque distinctio ipsum illum nobis deserunt quidem nam
-            ratione, commodi blanditiis veritatis? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Ducimus aliquid modi quos nesciunt,
-            recusandae veniam nostrum, doloremque ad autem, quae aspernatur
-            soluta accusantium eligendi consequatur quam perspiciatis expedita
-            animi consequuntur magni beatae inventore? Et alias facilis
-            architecto nemo maxime dolorum ab omnis sapiente tempore ipsum,
-            inventore repellendus rem vitae sed nam beatae delectus modi
-            similique adipisci eum repudiandae possimus obcaecati. Suscipit
-            facilis quam repellendus provident rem. Quae nulla reprehenderit
-            iure, fuga magni placeat illo sed. Temporibus, consequuntur alias
-            exercitationem voluptates autem voluptate reprehenderit obcaecati.
-            In facilis atque suscipit laudantium facere ea unde accusamus sint
-            optio dolorem harum, iste quis, animi nisi autem libero eligendi
-            modi culpa doloremque amet minus illo dolor. Doloribus sit et dicta
-            placeat quia commodi sunt assumenda facere fuga ducimus illo
-            possimus numquam eum aut impedit voluptatibus similique odit unde,
-            dolore asperiores vitae officiis sequi. Veniam, magnam provident
-            temporibus neque ipsam esse suscipit atque sit quod sed perferendis
-            delectus corrupti vel ullam asperiores? Ratione deleniti dolores
-            expedita temporibus vitae, minima doloremque corporis fugiat amet,
-            dolorem officiis a corrupti molestias beatae commodi optio
-            perspiciatis voluptas rem illum sequi mollitia perferendis minus ut
-            iusto. Asperiores debitis quo enim, dolore recusandae corrupti.
-            Natus iste aut, eligendi saepe et eum, illum modi nesciunt, ipsum
-            tempore magnam amet dignissimos voluptates fuga culpa reiciendis
-            quasi ex? Accusantium corporis odio impedit quidem quam inventore
-            animi officiis magni molestias quo eius saepe ut est omnis
-            laudantium distinctio suscipit excepturi enim ea soluta esse ex,
-            voluptatibus dicta quod. Amet, vero! Modi esse, voluptas vel dicta
-            magnam itaque beatae ipsam dolore reprehenderit natus animi fuga
-            aliquid nisi voluptates ut voluptatem fugiat commodi accusamus earum
-            voluptate? Pariatur quasi nulla, quam quis incidunt error quaerat
-            vero nihil recusandae modi? Quod et mollitia similique corporis. At,
-            dignissimos ex ut fugit doloremque porro cupiditate id corrupti,
-            rerum repellendus molestias odio alias sed sint quaerat debitis,
-            obcaecati atque in quo! Mollitia nesciunt nemo eveniet saepe esse
-            aspernatur in soluta illum labore ratione possimus quae magni
-            consequatur, temporibus ipsam quibusdam, omnis eos voluptatum, modi
-            voluptatem doloremque? Porro, consectetur deleniti. Nam repellendus
-            repellat esse magnam architecto perferendis?`
+para.innerText = `• Overview of the Topic
+• Key Ideas and Explanations
+• Supporting Details and Examples
+• Final Thoughts and Observations
+
+>
+\t Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae fugit voluptate quis eveniet molestias illum doloribus esse. Illum nobis consequatur ab recusandae a rerum veniam quibusdam hic quidem accusamus. Cum eius voluptates harum at numquam est doloremque aperiam repellendus expedita sit ea sunt laborum accusantium consectetur id ipsa labore soluta aliquid debitis dignissimos doloribus in.
+
+>
+\t Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus aliquid modi quos nesciunt recusandae veniam nostrum doloremque ad autem quae aspernatur soluta accusantium eligendi consequatur quam perspiciatis expedita animi consequuntur magni beatae inventore. Et alias facilis architecto nemo maxime dolorum ab omnis sapiente tempore ipsum inventore repellendus rem vitae sed nam beatae delectus modi similique adipisci eum repudiandae possimus obcaecati.
+
+>
+\t Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facilis quam repellendus provident rem quae nulla reprehenderit iure fuga magni placeat illo sed temporibus consequuntur alias exercitationem voluptates autem voluptate reprehenderit obcaecati. In facilis atque suscipit laudantium facere ea unde accusamus sint optio dolorem harum iste quis animi nisi autem libero eligendi modi culpa doloremque amet minus illo.
+
+>
+\t Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores debitis quo enim dolore recusandae corrupti natus iste aut eligendi saepe et eum illum modi nesciunt ipsum tempore magnam amet dignissimos voluptates fuga culpa reiciendis quasi ex. Accusantium corporis odio impedit quidem quam inventore animi officiis magni molestias quo eius saepe ut est omnis laudantium distinctio suscipit excepturi enim ea soluta esse.`;
